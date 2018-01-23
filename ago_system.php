d3d406eeb4696718d7271bd1965b9ee6f26d8e44
nicolousJ <35724970+nicolousJ@users.noreply.github.com>
2018-01-23 12:53:27 +0300

    Add files via upload
    i added new file

---
+++ ago_system.php
@@ -0,0 +1,30 @@
+ 
+
+// pass the id of the particular post to get the time when the post was created
+
+// provide the created_at column of the post table and reference the diffForHumans() method;
+
+
+
+public function posted_at($id){
+
+        $post = Post::find($id);
+         $post->touch();
+    return $post->created_at->diffForHumans();
+
+    }
+
+
+// pass the id of the particular comment to get the time when the comment was created
+
+// provide the created_at column of the comment table and reference the diffForHumans() method;
+
+
+
+    public function commented_at($id){
+
+        $post = Comment::find($id);
+         $post->touch();
+    return $post->created_at->diffForHumans();
+
+    }
